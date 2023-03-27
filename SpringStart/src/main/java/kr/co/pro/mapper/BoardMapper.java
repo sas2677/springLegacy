b434b14d7f54102e9026866b52692bbad0d71a63
048 <048@A-20>
2023-03-27 17:45:49 +0900

    my batis 연결

--- SpringStart/src/main/java/kr/co/pro/mapper/BoardMapper.java
+++ SpringStart/src/main/java/kr/co/pro/mapper/BoardMapper.java
@@ -2,9 +2,12 @@ package kr.co.pro.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import kr.co.pro.Entity.Board;
 
-//@mapper - mybatis API
+
+@Mapper //@mapper - mybatis API
 public interface BoardMapper {
 	//전체 리스트
 	public List<Board> listAll();
